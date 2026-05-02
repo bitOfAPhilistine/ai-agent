f90a03f27a31d8f646921639c3ae7d9c45ad13bb
BitofaPhilistine <philipgowell@gmail.com>
2026-05-01 20:23:44 -0400

    added test for api

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+if not api_key:
+    raise RuntimeError("No API key found!")
+client = genai.Client(api_key=api_key)
+
+
 def main():
-    print("Hello from ai-agent!")
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+    print(response.text)
 
 
 if __name__ == "__main__":
