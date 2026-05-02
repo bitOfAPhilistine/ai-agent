aba8aaea43a37b736d84e2db0475cfd9b637fd30
BitofaPhilistine <philipgowell@gmail.com>
2026-05-01 20:33:22 -0400

    Added token counters and custom prompting

--- main.py
+++ main.py
@@ -10,9 +10,16 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    prompt = input("Input prompt: ")
     response = client.models.generate_content(
         model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.")
+        contents=prompt)
+    
+    if not response.usage_metadata:
+        raise RuntimeError("API request failed")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
     print(response.text)
 
 
