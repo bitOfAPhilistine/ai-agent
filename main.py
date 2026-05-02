2464203299a78b88bf13a98a12348fb24f357fd1
BitofaPhilistine <philipgowell@gmail.com>
2026-05-01 23:31:55 -0400

    LES GOOOOOOOOO

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import os
+import sys
 import argparse
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
-from get_file_content import schema_get_file_content, get_file_content
-from get_files_info import schema_get_files_info, get_files_info
-from write_file import schema_write_file, write_file
-from run_python_file import schema_run_python_file, run_python_file
+from functions.get_file_content import schema_get_file_content, get_file_content
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.write_file import schema_write_file, write_file
+from functions.run_python_file import schema_run_python_file, run_python_file
 from config import SYSTEM_PROMPT
 
 
@@ -23,28 +24,91 @@ args = parser.parse_args()
 messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
 
 available_functions = types.Tool(
-    function_declarations=[schema_get_files_info],
+    function_declarations=[schema_get_file_content, schema_get_files_info, schema_write_file, schema_run_python_file],
 )
+function_map = {
+    "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
+    "write_file": write_file,
+    "run_python_file": run_python_file
+}
 
 def main():
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents=messages,
-        config=types.GenerateContentConfig(
-            system_instruction=SYSTEM_PROMPT,
-            tools=available_functions))
-    
-    if not response.usage_metadata:
-        raise RuntimeError("API request failed")
+    for _ in range(20):
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=messages,
+            config=types.GenerateContentConfig(
+                system_instruction=SYSTEM_PROMPT,
+                tools=[available_functions]))
 
-    if args.verbose:
-        print(f"User prompt: {args.user_prompt}")
-        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print(response.text)
+        if not response.usage_metadata:
+            raise RuntimeError("API request failed")
 
-    for function_call in response.function_calls:
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+        
+        if response.candidates:
+            for candidate in response.candidates:
+                messages.append(candidate.content)
+
+        function_results = []
+        if response.function_calls:
+            for function_call in response.function_calls:
+                call_result = call_function(function_call, args.verbose)
+                if len(call_result.parts) <= 0:
+                    raise Exception
+                if not call_result.parts[0].function_response:
+                    raise Exception
+                if not call_result.parts[0].function_response.response:
+                    raise Exception
+                
+                function_results.append(call_result.parts[0])
+                if args.verbose:
+                    print(f"-> {call_result.parts[0].function_response.response}")
+            messages.append(types.Content(role="user", parts=function_results))
+        else:
+            print("Final response:")
+            print(response.text)
+            sys.exit(0)
+    print("Loop limit reached!")
+    sys.exit(1)
+
+
+def call_function(function_call, verbose=False):
+    if verbose:
         print(f"Calling function: {function_call.name}({function_call.args})")
+    else:
+        print(f" - Calling function: {function_call.name}")
+    
+    name = function_call.name or ""
+
+    if name not in function_map:
+        return types.Content(
+            role="tool",
+            parts=[
+                types.Part.from_function_response(
+                    name=name,
+                    response={"error": f"Unknown function: {name}"}
+                )
+            ]
+        )
+    
+    args = dict(function_call.args) if function_call.args else {}
+    args["working_directory"] = "./calculator"
+
+    result = function_map[name](**args)
+    return types.Content(
+        role="tool",
+        parts=[
+            types.Part.from_function_response(
+                name=name,
+                response={"result": result},
+            )
+        ],
+    )
 
 
 if __name__ == "__main__":
